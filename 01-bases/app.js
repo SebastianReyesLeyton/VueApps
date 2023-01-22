fedbde6bd7172d36d76308ef2279a792414e0ad0
SebastianReyesLeyton <sebas.reyes2002@hotmail.com>
2023-01-22 18:39:54 -0500

    Uso de funciones, manejo de estado local

--- 01-bases/app.js
+++ 01-bases/app.js
@@ -3,12 +3,25 @@ const app = Vue.createApp({
         <h1> {{ message }} </h1>
         <p> {{ quote }} </p>
         <blockquote> -{{ author }} </blockquote>
+
+        <button v-on:click="changeQuote"> Change quote </button>
     `,
     data () {
         return {
             message: 'Hola mundo',
             quote: `I'm Batman`,
             author: `Bruce Wayne`
+        }
+    },
+    methods: {
+        changeQuote ( event ) {
+            console.log('Hola mundo', event);
+            this.author = 'Juan Sebastian Reyes Leyton';
+
+            this.capitalize();
+        },
+        capitalize () {
+            this.quote = this.quote.toUpperCase();
         }
     }
 });
