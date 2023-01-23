0b9f854d84aa6994d0abb56a6d5ffbcc5437d0cf
SebastianReyesLeyton <sebas.reyes2002@hotmail.com>
2023-01-23 11:43:54 -0500

    RoutingApp: Enrutamiento anidado

--- 05-router-cycles/src/router/router.js
+++ 05-router-cycles/src/router/router.js
@@ -3,26 +3,36 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 const routes = [
     { 
         path: '/',
-        redirect: '/home'
+        redirect: '/pokemon'
     },
-    { 
-        path: '/home',
-        name: 'home',
-        component: () => import(/* webpackChunkName: "ListPage" */ '@/modules/pokemon/pages/ListPage') 
-    },
-    { 
-        path: '/pokemonAbout',
-        name: 'pokemon-about',
-        component: () => import(/* webpackChunkName: "AboutPage" */ '@/modules/pokemon/pages/AboutPage')
-    },
-    { 
-        name: 'pokemon-id',
-        path: '/pokemonId/:pokemonId', 
-        component: () => import(/* webpackChunkName: "PokemonPage" */ '@/modules/pokemon/pages/PokemonPage'),
-        props: ( route ) => {
-            const { pokemonId } = route.params
-            return isNaN( Number(pokemonId) ) ? { id: 1 } : { id: Number(pokemonId) }
-        }
+    {
+        path: '/pokemon',
+        component: () => import(/* webpackChunkName: "PokemonLayout" */ '@/modules/pokemon/layouts/PokemonLayout'),
+        children: [
+            { 
+                path: 'home',
+                name: 'pokemon-home',
+                component: () => import(/* webpackChunkName: "ListPage" */ '@/modules/pokemon/pages/ListPage') 
+            },
+            { 
+                path: 'about',
+                name: 'pokemon-about',
+                component: () => import(/* webpackChunkName: "AboutPage" */ '@/modules/pokemon/pages/AboutPage')
+            },
+            { 
+                path: 'id/:pokemonId', 
+                name: 'pokemon-id',
+                component: () => import(/* webpackChunkName: "PokemonPage" */ '@/modules/pokemon/pages/PokemonPage'),
+                props: ( route ) => {
+                    const { pokemonId } = route.params
+                    return isNaN( Number(pokemonId) ) ? { id: 1 } : { id: Number(pokemonId) }
+                }
+            },
+            {
+                path: '',
+                redirect: { name: 'pokemon-home' }
+            }
+        ]
     },
     { 
         path: '/:pathMatch(.*)*', 
