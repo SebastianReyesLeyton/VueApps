fde5bd869807076d812297bf8171f657a1eb4870
SebastianReyesLeyton <sebas.reyes2002@hotmail.com>
2023-01-23 11:54:39 -0500

    RoutingApp: Ejercicio creando otro Layout

--- 05-router-cycles/src/router/router.js
+++ 05-router-cycles/src/router/router.js
@@ -34,6 +34,27 @@ const routes = [
             }
         ]
     },
+    {
+        path: '/dbz',
+        name: 'dbz',
+        component: () => import(/* webpackChunkName: "DBZLayout" */ '@/modules/dbz/layouts/DragonBallLayout'),
+        children: [
+            {
+                path: 'characters',
+                name: 'dbz-characters',
+                component: () => import(/* webpackChunkName: "DBZLayout" */ '@/modules/dbz/pages/Characters')
+            },
+            {
+                path: 'about',
+                name: 'dbz-about',
+                component: () => import(/* webpackChunkName: "DBZLayout" */ '@/modules/dbz/pages/About')
+            },
+            {
+                path: '',
+                redirect: { name: 'dbz-characters' }
+            }
+        ]
+    },
     { 
         path: '/:pathMatch(.*)*', 
         component: () => import(/* webpackChunkName: "NotFound" */ '@/modules/shared/pages/NoPageFound')
