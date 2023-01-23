e10e2f8625e5dd16725f7bd3ccdab7ccc27c8ec5
SebastianReyesLeyton <sebas.reyes2002@hotmail.com>
2023-01-22 21:00:20 -0500

    Funciones computadas (optimización solamente hace un llamado)

--- 03-vue-cli/fundaments/src/components/Counter.vue
+++ 03-vue-cli/fundaments/src/components/Counter.vue
@@ -1,6 +1,6 @@
 <template>
   <h2> Counter !! </h2>
-  <p> {{ counter }} </p>
+  <p> {{ counter }} <sup>2</sup> = {{ squareCounter }} </p>
 </template>
 
 <script>
@@ -9,6 +9,16 @@
         data () {
             return {
                 counter: 5
+            }
+        },
+        methods: {
+            getSquareValue () {
+                return this.counter * this.counter;
+            }
+        },
+        computed: {
+            squareCounter () {
+                return this.counter * this.counter;
             }
         }
     }
