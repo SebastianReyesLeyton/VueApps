05bf6305d41ab849bfa33cda317561693f2d5aca
SebastianReyesLeyton <sebas.reyes2002@hotmail.com>
2023-01-22 22:37:32 -0500

    IndecisionApp Tests: Comparandos y buscando el valor de una etiquera

--- 03-vue-cli/fundaments/tests/unit/components/counter.spec.js
+++ 03-vue-cli/fundaments/tests/unit/components/counter.spec.js
@@ -6,10 +6,20 @@ import Counter from '@/components/Counter';
 describe('Counter Component', () => {
 
     test('Should to be match with snapshot', () => {
+        const wrapper = shallowMount( Counter );
+        expect( wrapper.html() ).toMatchSnapshot();
+    });
 
+    test('h2 must to have the default value "Counter"', () => {
         const wrapper = shallowMount( Counter );
 
-        expect( wrapper.html() ).toMatchSnapshot();
+        expect( wrapper.find('h2').exists() ).toBeTruthy();
+        
+        /** Seek the first */
+        const h2 = wrapper.find('h2')
+        console.log(h2.text())
+
+        expect(h2.text()).toBe('Counter');
     });
 
 });
