3a52c03445c76638dae063bc0ab63ce5def7b710
SebastianReyesLeyton <sebas.reyes2002@hotmail.com>
2023-01-23 10:44:25 -0500

    RoutingApp: Ciclos de vida del componente AboutPagePokemon

--- 05-router-cycles/src/modules/pokemon/pages/AboutPage.vue
+++ 05-router-cycles/src/modules/pokemon/pages/AboutPage.vue
@@ -1,10 +1,61 @@
 <template>
     <h1>About page</h1>
+    <h3>{{ name }}</h3>
+    <button @click="onChangeName">Cambiar Nombre</button>
 </template>
 
 <script>
     export default {
-
+        data () {
+            return {
+                name: 'Juan'
+            }
+        },
+        methods: {
+            onChangeName () {
+                this.name = 'Melissa'
+            }
+        },
+        beforeCreate () {
+            console.log("beforeCreate");
+        },
+        created () {
+            // Peticiones HTTP
+            console.log("created");
+        },
+        beforeMount () {
+            console.log("beforeMount");
+        },
+        mounted () {
+            console.log("mounted");
+        },
+        beforeUpdate () {
+            console.log("beforeUpdate");
+        },
+        updated () {
+            console.log("updated");
+        },
+        activated () {
+            console.log("activated");
+        },
+        deactivated () {
+            console.log("deactivated");
+        },
+        beforeUnmount() {
+            console.log("beforeUnmount");
+        },
+        unmounted () {
+            console.log("unmounted");
+        },
+        errorCaptured () {
+            console.log("errorCaptured");
+        },
+        renderTracked () {
+            console.log("renderTracked");
+        },
+        renderTriggered () {
+            console.log("renderTriggered");
+        }
     }
 </script>
 
