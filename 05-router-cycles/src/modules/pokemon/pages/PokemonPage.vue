9fc45ec61fe3179cec0a9844f4e12212c2cbd52a
SebastianReyesLeyton <sebas.reyes2002@hotmail.com>
2023-01-23 11:10:56 -0500

    RoutingApp: Consumo API pokemon, manejo de error y redirección

--- 05-router-cycles/src/modules/pokemon/pages/PokemonPage.vue
+++ 05-router-cycles/src/modules/pokemon/pages/PokemonPage.vue
@@ -1,5 +1,8 @@
 <template>
-    <h1>Pokemon page: {{ id }}</h1>
+    <h1>Pokemon: {{ id }}</h1>
+    <div v-if="pokemon">
+        <img :src="pokemon.sprites.front_default" :alt="pokemon.name">
+    </div>
 </template>
 
 <script>
@@ -8,6 +11,30 @@
             id: {
                 type: Number,
                 required: true
+            }
+        },
+        data () {
+            return {
+                pokemon: null
+            }
+        },
+        created () {
+            this.getPokemon();
+        },
+        methods: {
+            async getPokemon () {
+                try {
+                    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.id}`).then( r => r.json() )
+                    this.pokemon = pokemon;    
+                } catch (error) {
+                    this.$router.push('/');
+                    console.log('No hay nada que hacer aquí');
+                }
+            } 
+        },
+        watch: {
+            id () {
+                this.getPokemon();
             }
         }
     }
--- 05-router-cycles/src/modules/shared/components/Navbar.vue
+++ 05-router-cycles/src/modules/shared/components/Navbar.vue
@@ -2,7 +2,7 @@
     <div>
         <router-link to="/">Pokemon List</router-link>
         <router-link to="/about">Pokemon About</router-link>
-        <router-link to="/id">Pokemon id</router-link>
+        <router-link :to="{ name: 'pokemon-id', params: { pokemonId: 20 } }">Pokemon id</router-link>
     </div>
 </template>
 
