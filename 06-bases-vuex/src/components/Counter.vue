047225875f04320648af2db0f4a114b8a5966c31
SebastianReyesLeyton <sebas.reyes2002@hotmail.com>
2023-01-23 20:18:17 -0500

    CounterApp Vuex: Obtención y registro de estados

---
+++ 06-bases-vuex/src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>Counter - Vuex</h1>
+    <h2>Direct access: {{ $store.state.count }}</h2>
+    <h1>Map state</h1>
+    <h2>Map state: {{ count }}</h2>
+    <h2>Last mutation: {{ lastMutation }}</h2>
+
+</template>
+
+<script>
+
+    import { mapState } from 'vuex';
+
+    export default {
+        name: 'Counter',
+        computed: {
+            ...mapState({
+                count: state => state.count,
+                lastMutation: state => state.lastMutation
+            })
+        }
+    }
+</script>
+
+<style>
+
+</style>
