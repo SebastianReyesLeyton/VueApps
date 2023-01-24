3350f2e4b297ad6a238e33ca0bd0366db81fb690
SebastianReyesLeyton <sebas.reyes2002@hotmail.com>
2023-01-24 00:03:31 -0500

    JournalApp: Creación inicial

---
+++ 07-journal-app/app/src/modules/daybook/components/Fab.vue
@@ -0,0 +1,29 @@
+<template>
+    <button class="btn btn-primary">
+        <i class="fa fa-2x fa-plus"></i>
+    </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    button {
+        position: fixed;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        
+        bottom: 1.5rem;
+        right: 1.5rem;
+        
+        width: 60px;
+        height: 60px;
+
+        border-radius: 100%;
+    }
+</style>
